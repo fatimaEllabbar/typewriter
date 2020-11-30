290f20365c44cb5be28d7132ca2b90d48a7c06ec
vagrant <fatima.ellabbar@gmail.com>
2020-11-30 17:48:13 +0000

    playing with setTimeout

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+
+const sentence = "hello there from lighthouse labs";
+let i = 0;
+for (const char of sentence) {
+ 
+  setTimeout(() => {
+    process.stdout.write(char);
+  },i);
+
+  i += 50;
+}
+setTimeout(() => {
+  console.log();
+},i);
+
+
